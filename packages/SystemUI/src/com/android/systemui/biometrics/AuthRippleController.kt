175430937994bbd85a5a16c4aa4cf84f48257830
Rishawn <70256146+Stealth1226@users.noreply.github.com>
2022-01-10 09:47:15 +0530

    base: disable ripple effect on unlock
        From: Nauval Rizky <enuma.alrizky@gmail.com>
        Date: Wed, 19 Jan 2022 08:31:56 +0700
        Subject: SystemUI: Fix method for disabling unlock ripple
         animation
    
        NotificationShadeWindowController's are not gracefully closed, which
        leads to top parts of the screen is being obscured by invisible shade window.
        And also, "ripple" is actually still exists due to lightRevealScrim are
        being started by onKeyguardFadingAwayChanged, which is not what user expect
        when they want to disable the effect.
    
        So in order to handle those cases, lets intercept it from the
        AuthRippleController instead.
    
        Signed-off-by: alanndz <alanndz7@gmail.com>
        Signed-off-by: Joey Huab <joey@evolution-x.org>
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    @neobuddy89:
    * Reverse logic. Enabled animation by default.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/AuthRippleController.kt
+++ packages/SystemUI/src/com/android/systemui/biometrics/AuthRippleController.kt
@@ -218,65 +218,13 @@ class AuthRippleController @Inject constructor(
     }
 
     private fun showUnlockedRipple() {
-        notificationShadeWindowController.setForcePluginOpen(true, this)
-
-        // This code path is not used if the KeyguardTransitionRepository is managing the light
-        // reveal scrim.
-        if (!lightRevealMigration()) {
-            if (statusBarStateController.isDozing || biometricUnlockController.isWakeAndUnlock) {
-                circleReveal?.let {
-                    lightRevealScrim.revealAmount = 0f
-                    lightRevealScrim.revealEffect = it
-                    startLightRevealScrimOnKeyguardFadingAway = true
-                }
-            }
-        }
-
-        mView.startUnlockedRipple(
-            /* end runnable */
-            Runnable {
-                notificationShadeWindowController.setForcePluginOpen(false, this)
-            }
-        )
+        return
     }
 
     override fun onKeyguardFadingAwayChanged() {
-        if (lightRevealMigration()) {
-            return
-        }
-
-        if (keyguardStateController.isKeyguardFadingAway) {
-            if (startLightRevealScrimOnKeyguardFadingAway) {
-                lightRevealScrimAnimator?.cancel()
-                lightRevealScrimAnimator = ValueAnimator.ofFloat(.1f, 1f).apply {
-                    interpolator = Interpolators.LINEAR_OUT_SLOW_IN
-                    duration = RIPPLE_ANIMATION_DURATION
-                    startDelay = keyguardStateController.keyguardFadingAwayDelay
-                    addUpdateListener { animator ->
-                        if (lightRevealScrim.revealEffect != circleReveal) {
-                            // if something else took over the reveal, let's cancel ourselves
-                            cancel()
-                            return@addUpdateListener
-                        }
-                        lightRevealScrim.revealAmount = animator.animatedValue as Float
-                    }
-                    addListener(object : AnimatorListenerAdapter() {
-                        override fun onAnimationEnd(animation: Animator) {
-                            // Reset light reveal scrim to the default, so the CentralSurfaces
-                            // can handle any subsequent light reveal changes
-                            // (ie: from dozing changes)
-                            if (lightRevealScrim.revealEffect == circleReveal) {
-                                lightRevealScrim.revealEffect = LiftReveal
-                            }
-
-                            lightRevealScrimAnimator = null
-                        }
-                    })
-                    start()
-                }
-                startLightRevealScrimOnKeyguardFadingAway = false
-            }
-        }
+        lightRevealScrim.revealAmount = 1f
+        startLightRevealScrimOnKeyguardFadingAway = false
+        return
     }
 
     /**
