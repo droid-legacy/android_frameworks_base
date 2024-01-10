1dac08d70a3586549289c0142261b366c9184af4
SirRGB <sirrgb@proton.me>
2024-01-10 21:51:27 +0100

    Revert "Add separate tunables for navbar layout customization"
    This reverts commit 38a5d305f4b2244c75b3f04e6c96d0fb965ad34c.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -10242,18 +10242,6 @@ public final class Settings {
         public static final String BACK_GESTURE_HAPTIC = "back_gesture_haptic";
 
         /**
-         * Select from different navigation bar layouts
-         * @hide
-         */
-        public static final String NAVBAR_LAYOUT_VIEWS = "navbar_layout_views";
-
-        /**
-         * Inverse navigation bar layout
-         * @hide
-         */
-        public static final String NAVBAR_INVERSE_LAYOUT = "navbar_inverse_layout";
-
-        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
