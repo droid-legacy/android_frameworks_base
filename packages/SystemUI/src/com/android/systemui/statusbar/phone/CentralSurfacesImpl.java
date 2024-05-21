faa805fb07e5a403a35d509f46c51fb773c06bc0
SirRGB <sirrgb@proton.me>
2024-05-21 18:08:18 +0200

    Revert "Use FORCE_SHOW_NAVBAR to actually toggle navbar"
    This reverts commit 6f3e7f617f6387c24f32babcccccc2d539b81cde.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -106,7 +106,6 @@ import com.android.internal.logging.UiEventLoggerImpl;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.internal.statusbar.IStatusBarService;
 import com.android.internal.statusbar.RegisterStatusBarResult;
-import com.android.internal.util.crdroid.Utils;
 import com.android.keyguard.AuthKeyguardMessageArea;
 import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.keyguard.KeyguardUpdateMonitorCallback;
@@ -881,6 +880,18 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
 
         mColorExtractor.addOnColorsChangedListener(mOnColorsChangedListener);
 
+        mNeedsNavigationBar = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_showNavigationBar);
+        // Allow a system property to override this. Used by the emulator.
+        // See also hasNavigationBar().
+        String navBarOverride = SystemProperties.get("qemu.hw.mainkeys");
+        if ("1".equals(navBarOverride)) {
+            mNeedsNavigationBar = false;
+        } else if ("0".equals(navBarOverride)) {
+            mNeedsNavigationBar = true;
+        }
+
+
         mWindowManager = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
 
         mDisplay = mContext.getDisplay();
@@ -908,7 +919,6 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
         RegisterStatusBarResult result = null;
         try {
             result = mBarService.registerStatusBar(mCommandQueue);
-            mResult = result;
         } catch (RemoteException ex) {
             ex.rethrowFromSystemServer();
         }
@@ -1210,6 +1220,8 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
         mStatusBarInitializer.initializeStatusBar();
 
         mStatusBarTouchableRegionManager.setup(getNotificationShadeWindowView());
+
+        createNavigationBar(result);
 
         mAmbientIndicationContainer = getNotificationShadeWindowView().findViewById(
                 R.id.ambient_indication_container);
@@ -2901,17 +2913,17 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
             case FORCE_SHOW_NAVBAR:
                 if (mDisplayId != Display.DEFAULT_DISPLAY || mWindowManagerService == null)
                     return;
-                boolean mNavbarVisible =
-                        TunerService.parseIntegerSwitch(newValue, Utils.hasNavbarByDefault(mContext));
+                boolean forcedVisibility = mNeedsNavigationBar ||
+                    TunerService.parseIntegerSwitch(newValue, false);
                 boolean hasNavbar = getNavigationBarView() != null;
                 mContext.getMainExecutor().execute(() -> {
-                    if (mNavbarVisible) {
-                        if (!hasNavbar && mResult != null) {
-                            createNavigationBar(mResult);
+                    if (forcedVisibility) {
+                        if (!hasNavbar) {
+                            mNavigationBarController.onDisplayReady(mDisplayId);
                         }
                     } else {
                         if (hasNavbar) {
-                            mNavigationBarController.removeNavigationBar(mDisplayId);
+                            mNavigationBarController.onDisplayRemoved(mDisplayId);
                         }
                     }
                 });
@@ -2925,7 +2937,6 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
 
     protected final CommandQueue mCommandQueue;
     protected IStatusBarService mBarService;
-    protected RegisterStatusBarResult mResult = null;
 
     // all notifications
     private final NotificationStackScrollLayout mStackScroller;
@@ -2941,6 +2952,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces,
     protected KeyguardManager mKeyguardManager;
     private final DeviceProvisionedController mDeviceProvisionedController;
 
+    private boolean mNeedsNavigationBar;
     private final NavigationBarController mNavigationBarController;
     private final AccessibilityFloatingMenuController mAccessibilityFloatingMenuController;
 
